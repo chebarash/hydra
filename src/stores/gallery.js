58abd52db7d628f474c2ae1b5150e1bc1b43835f
CheBarash <chebarashek@gmail.com>
2024-09-22 22:46:09 +0500

    THE TEAM*

--- src/stores/gallery.js
+++ src/stores/gallery.js
@@ -1,19 +1,19 @@
-import request from 'superagent'
-import examples from './examples.json'
-const sketches = []
+import request from "superagent";
+import examples from "./examples.json";
+const sketches = [];
 
-const license = `// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/`
+const license = `// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/`;
 
 export default class Gallery {
   constructor(callback, state, emitter) {
-    this.sketches = []
-    this.examples = []
-    this.current = null
-    this.code = null
-    this.exampleIndex = null
-    this.state = state
-    this.emitter = emitter
-    this.serverURL = this.state.serverURL === null ? '' : this.state.serverURL
+    this.sketches = [];
+    this.examples = [];
+    this.current = null;
+    this.code = null;
+    this.exampleIndex = null;
+    this.state = state;
+    this.emitter = emitter;
+    this.serverURL = this.state.serverURL === null ? "" : this.state.serverURL;
     // request.get('/sketches').end((err, res) => {
     //   console.log('got sketches', res.text, err)
     //   if(err) {
@@ -22,51 +22,54 @@ export default class Gallery {
     //     this.sketches = JSON.parse(res.text)
     //   }
 
-    this.examples = examples
-    this.setSketchFromURL(window.location.search, callback)
+    this.examples = examples;
+    this.setSketchFromURL(window.location.search, callback);
     //  callback(this.code, this.foundSketch)
     // })
-    window.addEventListener('popstate', (event) => {
-      this.setSketchFromURL(window.location.search, callback)
+    window.addEventListener("popstate", (event) => {
+      this.setSketchFromURL(window.location.search, callback);
       // console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
     });
-    this.setRandomSketch = this.setRandomSketch.bind(this)
+    this.setRandomSketch = this.setRandomSketch.bind(this);
   }
 
   clear() {
-    this.current = null
-    this.code = null
+    this.current = null;
+    this.code = null;
     //this.exampleIndex = null
-    let newurl = window.location.protocol + '//' + window.location.host + window.location.pathname
-    window.history.pushState({ path: newurl }, '', newurl)
-    this.url = newurl
+    let newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState({ path: newurl }, "", newurl);
+    this.url = newurl;
   }
 
-  
   setSketchFromURL(path = window.location.search, callback) {
-    hush()
-    render(o0)
+    hush();
+    render(o0);
 
-    let searchParams = new URLSearchParams(path)
-    this.searchParams = searchParams
-    let base64Code = searchParams.get('code')
+    let searchParams = new URLSearchParams(path);
+    this.searchParams = searchParams;
+    let base64Code = searchParams.get("code");
     //  if(!base64Code) base64Code = searchParams.get('id') // backwards compatibility with earlier form of naming. id is now called code
-    let sketch_id = searchParams.get('sketch_id')
-    let showCode = searchParams.get('showCode')
+    let sketch_id = searchParams.get("sketch_id");
+    let showCode = searchParams.get("showCode");
 
-    console.log('code is', base64Code)
-    let code = ''
+    console.log("code is", base64Code);
+    let code = "";
     //console.log("id", sketch_id, "code", base64Code)
 
     // boolean to determine whether a sketch was found based on the URL, either through looking through the database or rendering the code
-    this.foundSketch = false
+    this.foundSketch = false;
     // if contains a sketch id, set sketch from id
     if (sketch_id) {
-      var sketch = this.getExampleById(sketch_id)
+      var sketch = this.getExampleById(sketch_id);
       if (sketch) {
         // console.log('found', sketch)
-        this.setSketch(sketch)
-        callback(this.code, false)
+        this.setSketch(sketch);
+        callback(this.code, false);
       } else {
         request
           // .get('/sketchById')
@@ -75,28 +78,27 @@ export default class Gallery {
           .end((err, res) => {
             // console.log('got sketches', res.text, err)
             if (err) {
-              console.log('err getting sketches', err)
-              this.setSketchFromCode(base64Code, callback)
+              console.log("err getting sketches", err);
+              this.setSketchFromCode(base64Code, callback);
             } else {
-              this.sketches = JSON.parse(res.text)
+              this.sketches = JSON.parse(res.text);
               if (this.sketches.length > 0) {
-                this.setSketch(this.sketches[0])
+                this.setSketch(this.sketches[0]);
                 //this.code = this.decodeBase64(this.sketches[0].code)
-                this.foundSketch = true
-                callback(this.code, this.foundSketch)
+                this.foundSketch = true;
+                callback(this.code, this.foundSketch);
               } else {
-                this.setSketchFromCode(base64Code, callback)
+                this.setSketchFromCode(base64Code, callback);
               }
             }
-          })
+          });
       }
-
     } else {
-      this.setSketchFromCode(base64Code, callback)
+      this.setSketchFromCode(base64Code, callback);
     }
-    if (showCode === 'false') {
-      this.emitter.emit('ui: hide all')
-      this.emitter.emit('hide info')
+    if (showCode === "false") {
+      this.emitter.emit("ui: hide all");
+      this.emitter.emit("hide info");
     }
     //
     //   // console.log('found ', sketch)
@@ -125,102 +127,100 @@ export default class Gallery {
 
   setSketchFromCode(base64Code, callback) {
     if (base64Code) {
-      this.code = this.decodeBase64(base64Code)
-      this.foundSketch = true
+      this.code = this.decodeBase64(base64Code);
+      this.foundSketch = true;
     } else {
-      this.setRandomSketch()
+      this.setRandomSketch();
     }
-    callback(this.code, this.foundSketch)
+    callback(this.code, this.foundSketch);
   }
 
-  saveImage() {
-
-  }
+  saveImage() {}
 
   setToURL(params) {
     //       console.log(base64)
     // console.log('params', params)
     // keep code in url for backwards compatibility / compatibility between local and public versions
-    this.searchParams.delete('sketch_id')
-    this.searchParams.delete('code')
-    let url_params
+    this.searchParams.delete("sketch_id");
+    this.searchParams.delete("code");
+    let url_params;
     if (params.sketch_id) {
-      url_params = `sketch_id=${params.sketch_id}`
-      this.searchParams.append('sketch_id', params.sketch_id)
+      url_params = `sketch_id=${params.sketch_id}`;
+      this.searchParams.append("sketch_id", params.sketch_id);
     } else {
-      url_params = `sketch_id=${params.sketch_id}&code=${params.code}`
-      this.searchParams.append('sketch_id', params.sketch_id)
-      this.searchParams.append('code', params.code)
+      url_params = `sketch_id=${params.sketch_id}&code=${params.code}`;
+      this.searchParams.append("sketch_id", params.sketch_id);
+      this.searchParams.append("code", params.code);
     }
 
-    let newurl = window.location.protocol + '//' +
-      window.location.host + window.location.pathname + '?' + this.searchParams.toString()
-    window.history.replaceState({ path: newurl }, '', newurl)
-    this.url = newurl
+    let newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname +
+      "?" +
+      this.searchParams.toString();
+    window.history.replaceState({ path: newurl }, "", newurl);
+    this.url = newurl;
   }
 
   encodeBase64(text) {
-    return btoa(encodeURIComponent(text))
+    return btoa(encodeURIComponent(text));
   }
   decodeBase64(base64Code) {
-    return decodeURIComponent(atob(base64Code))
+    return decodeURIComponent(atob(base64Code));
   }
 
   setSketch(sketch) {
-    let code = this.decodeBase64(sketch.code)
+    let code = this.decodeBase64(sketch.code);
     if (code.indexOf(license) < 0)
-      code =
-        `${license}
+      code = `${license}
 ${code}
-`
-    this.code = code
-    this.current = sketch
+`;
+    this.code = code;
+    this.current = sketch;
     //  this.setToURL('sketch_id', sketch._id)
     // let params = Object.keys(sketch).map( (key) => {
     //   return {label: key, value: sketch[key]}
     // })
-    this.setToURL(sketch)
+    this.setToURL(sketch);
   }
 
   setRandomSketch() {
     // if there are sketches, set code from sketch, otherwise generate random
     //console.log("examples length", this.examples, this.exampleIndex)
     if (this.examples.length > 0) {
-      let index
-      // if(this.exampleIndex === null) {
-      index = Math.floor(Math.random() * this.examples.length)
-      while (index === this.exampleIndex) {
-        index = Math.floor(Math.random() * this.examples.length)
-      }
-      // } else {
-      //   index = this.exampleIndex +1
-      //   if(index >= this.examples.length) index = 0
-      // }
-      this.exampleIndex = index
-      //  console.log('example is', this.examples[rand])
-      this.setSketch(this.examples[index])
+      this.setSketch(this.examples[this.examples.length - 1]);
     } else {
-      var startString = 'osc(' + 2 + Math.floor(Math.pow(10, Math.random() * 2)) + ')'
-      startString += '.color(' + Math.random().toFixed(2) + ',' + Math.random().toFixed(2) + ',' + Math.random().toFixed(2) + ')'
-      startString += '.rotate(' + Math.random().toFixed(2) + ')'
-      startString += '.out(o0)'
-      this.code = startString
+      var startString =
+        "osc(" + 2 + Math.floor(Math.pow(10, Math.random() * 2)) + ")";
+      startString +=
+        ".color(" +
+        Math.random().toFixed(2) +
+        "," +
+        Math.random().toFixed(2) +
+        "," +
+        Math.random().toFixed(2) +
+        ")";
+      startString += ".rotate(" + Math.random().toFixed(2) + ")";
+      startString += ".out(o0)";
+      this.code = startString;
     }
   }
 
   // shares via twitter
   shareSketch(code, hydra, name) {
     this.saveSketch(code, () => {
-      console.log("URL is", this.url, 'sketch is', this.current)
+      console.log("URL is", this.url, "sketch is", this.current);
       hydra.getScreenImage((img) => {
         request
           // .post('/image')
           .post(`${this.state.serverURL}/image`)
-          .attach('previewImage', img)
+          .attach("previewImage", img)
           .query({
             url: this.url,
             sketch_id: this.current.sketch_id,
-            name: name
+            name: name,
           })
           // .send({
           //   code: base64
@@ -228,13 +228,12 @@ ${code}
           //  .query(query)
           .end((err, res) => {
             if (err) {
-              console.log('error postingimage', err)
+              console.log("error postingimage", err);
             } else {
-              console.log('image response', res.text)
+              console.log("image response", res.text);
               //  self.setToURL([ { label: 'sketch_id', value: res.text}, {label: 'code', value: base64} ])
-
             }
-          })
+          });
         // var oReq = new XMLHttpRequest();
         // oReq.open("POST", "https://localhost:8000/image", true);
         // oReq.onload = function (oEvent) {
@@ -243,22 +242,22 @@ ${code}
         // };
         // oReq.send(img);
         //  console.log('got image', img)
-      })
-    })
+      });
+    });
   }
 
   saveSketch(code, callback) {
-    let self = this
+    let self = this;
     //console.log('saving in gallery', code)
-    let base64 = this.encodeBase64(code)
+    let base64 = this.encodeBase64(code);
     //  console.log('code is', base64)
 
     let query = {
       code: base64,
-      parent: this.current ? this.current.sketch_id : null
-    }
+      parent: this.current ? this.current.sketch_id : null,
+    };
 
-    console.log('saving in gallery', query)
+    console.log("saving in gallery", query);
     request
       // .post('/sketch')
       .post(`${this.state.serverURL}/sketch`)
@@ -268,41 +267,46 @@ ${code}
       .query(query)
       .end((err, res) => {
         if (err) {
-          console.log('error posting sketch', err)
-          if (callback) callback(err)
+          console.log("error posting sketch", err);
+          if (callback) callback(err);
         } else {
-          console.log('response', res.text)
+          console.log("response", res.text);
           //  self.setToURL([ { label: 'sketch_id', value: res.text}, {label: 'code', value: base64} ])
           self.setSketch({
             sketch_id: res.text,
-            code: base64
-          })
-          if (callback) callback(null)
+            code: base64,
+          });
+          if (callback) callback(null);
         }
-      })
+      });
   }
 
   saveLocally(code) {
-    let base64 = this.encodeBase64(code)
+    let base64 = this.encodeBase64(code);
 
-    this.searchParams.delete('sketch_id')
-    this.searchParams.delete('code')
+    this.searchParams.delete("sketch_id");
+    this.searchParams.delete("code");
 
-    this.searchParams.append('code', base64)
+    this.searchParams.append("code", base64);
     // keep code in url for backwards compatibility / compatibility between local and public versions
-    var url_params = this.searchParams.toString()
+    var url_params = this.searchParams.toString();
 
-    let newurl = window.location.protocol + '//' +
-      window.location.host + window.location.pathname + '?' + url_params
-    window.history.pushState({ path: newurl }, '', newurl)
-    this.url = newurl
+    let newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname +
+      "?" +
+      url_params;
+    window.history.pushState({ path: newurl }, "", newurl);
+    this.url = newurl;
   }
 
   getExampleById(id) {
     //console.log('looking for', id, this.examples, this.sketches)
-    var sketches = this.examples.filter((sketch) => sketch.sketch_id === id)
-    if (sketches.length <= 0) sketches = this.sketches.filter((sketch) => sketch.sketch_id === id)
-    return sketches[0]
+    var sketches = this.examples.filter((sketch) => sketch.sketch_id === id);
+    if (sketches.length <= 0)
+      sketches = this.sketches.filter((sketch) => sketch.sketch_id === id);
+    return sketches[0];
   }
 }
-
--- src/stores/store.js
+++ src/stores/store.js
@@ -1,96 +1,96 @@
-import repl from './repl-v2.js'
+import repl from "./repl-v2.js";
 // console.log('ENVIRONMENT IS', process.env.NODE_ENV)
 
 export default function store(state, emitter) {
-  state.showInfo = false
-  state.showUI = true
-  state.showExtensions = false
-  state.errorMessage = ''
-  state.isError = false
+  state.showInfo = false;
+  state.showUI = false;
+  state.showExtensions = false;
+  state.errorMessage = "";
+  state.isError = false;
 
   // if backend gallery endpoint supplied, then enable gallery functionality
-  const SERVER_URL = import.meta.env.VITE_SERVER_URL
-  state.serverURL = SERVER_URL !== undefined ? SERVER_URL : null
+  const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+  state.serverURL = SERVER_URL !== undefined ? SERVER_URL : null;
 
   window._reportError = (err) => {
-    state.errorMessage = err.message
-    state.isError = true
-    emitter.emit('render')
-  }
+    state.errorMessage = err.message;
+    state.isError = true;
+    emitter.emit("render");
+  };
 
-  emitter.on('load and eval code', (code, shouldUpdateURL = true) => {
-    emitter.emit('editor: load code', code)
-    emitter.emit('repl: eval', code)
-    if(shouldUpdateURL) emitter.emit('gallery: save to URL', code)
-  })
+  emitter.on("load and eval code", (code, shouldUpdateURL = true) => {
+    emitter.emit("editor: load code", code);
+    emitter.emit("repl: eval", code);
+    if (shouldUpdateURL) emitter.emit("gallery: save to URL", code);
+  });
 
-  emitter.on('repl: eval', (code = '', callback) => {
+  emitter.on("repl: eval", (code = "", callback) => {
     repl.eval(code, (info) => {
-      state.errorMessage = info.errorMessage
-      state.isError = info.isError
-      if(callback) callback(info.codeString, info.isError)
-      emitter.emit('render')
-    })
+      state.errorMessage = info.errorMessage;
+      state.isError = info.isError;
+      if (callback) callback(info.codeString, info.isError);
+      emitter.emit("render");
+    });
+  });
 
-  })
-
-  emitter.on('screencap', () => {
-    screencap()
-    const editor = state.editor.editor
-    const text = editor.getValue()
-    const data = new Blob([text], { type: 'text/plain' });
-    const a = document.createElement('a')
-    a.style.display = 'none'
-    let d = new Date()
-    a.download = `hydra-${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}-${d.getHours()}.${d.getMinutes()}.${d.getSeconds()}.js`
-    a.href = URL.createObjectURL(data)
-    a.click()
+  emitter.on("screencap", () => {
+    screencap();
+    const editor = state.editor.editor;
+    const text = editor.getValue();
+    const data = new Blob([text], { type: "text/plain" });
+    const a = document.createElement("a");
+    a.style.display = "none";
+    let d = new Date();
+    a.download = `hydra-${d.getFullYear()}-${
+      d.getMonth() + 1
+    }-${d.getDate()}-${d.getHours()}.${d.getMinutes()}.${d.getSeconds()}.js`;
+    a.href = URL.createObjectURL(data);
+    a.click();
 
     setTimeout(() => {
       window.URL.revokeObjectURL(a.href);
     }, 300);
-  })
+  });
 
   function clearAll() {
-    const editor = state.editor.editor
-    hush()
-    speed = 1
-    emitter.emit('gallery: clear')
-    editor.clear()
+    const editor = state.editor.editor;
+    hush();
+    speed = 1;
+    emitter.emit("gallery: clear");
+    editor.clear();
   }
 
-  emitter.on('clear all', () => {
-    clearAll()
-  })
+  emitter.on("clear all", () => {
+    clearAll();
+  });
 
+  emitter.on("ui: hide all", function () {
+    state.showUI = !state.showUI;
+    emitter.emit("render");
+  });
 
-  emitter.on('ui: hide all', function () {
-    state.showUI = !state.showUI
-    emitter.emit('render')
-  })
-
-  emitter.on('ui: toggle info', function (count) {
+  emitter.on("ui: toggle info", function (count) {
     if (state.showInfo) {
       // state.showInfo = false
       // state.showExtensions = false
-      emitter.emit('ui: hide info')
+      emitter.emit("ui: hide info");
     } else {
-      emitter.emit('ui: show info')
+      emitter.emit("ui: show info");
     }
     // state.showInfo = !state.showInfo
     //emitter.emit('render')
-  })
+  });
 
-  emitter.on('ui: show info', () => {
-    state.showInfo = true
-    emitter.emit('render')
-  })
+  emitter.on("ui: show info", () => {
+    state.showInfo = true;
+    emitter.emit("render");
+  });
 
-  emitter.on('ui: hide info', () => {
-    state.showInfo = false
-    state.showExtensions = false
-    emitter.emit('render')
-  })
+  emitter.on("ui: hide info", () => {
+    state.showInfo = false;
+    state.showExtensions = false;
+    emitter.emit("render");
+  });
 
   // emitter.on('hide info', function (count) {
   //   state.showInfo = false
@@ -98,22 +98,19 @@ export default function store(state, emitter) {
   //   emitter.emit('render')
   // })
 
-  emitter.on('ui: show extensions', () => {
-    state.showExtensions = true
-    state.showInfo = true
-    emitter.emit('extensions: select category')
-    emitter.emit('render')
-  })
+  emitter.on("ui: show extensions", () => {
+    state.showExtensions = true;
+    state.showInfo = true;
+    emitter.emit("extensions: select category");
+    emitter.emit("render");
+  });
 
-  emitter.on('ui: hide extensions', () => {
-    state.showExtensions = false
-    emitter.emit('render')
-  })
-
-
+  emitter.on("ui: hide extensions", () => {
+    state.showExtensions = false;
+    emitter.emit("render");
+  });
 
   // emitter.on('mutate sketch', function () {
 
   // })
 }
-
